7084cbd496f581a1f5404231684c9692f70319ae
yomaq <yomaq@bsjm.xyz>
2024-07-03 19:59:54 -0500

    pulumi-esc is now in nixpkgs

--- shell.nix
+++ shell.nix
@@ -6,9 +6,14 @@ pkgs.mkShell {
   packages = with pkgs; [
     pulumi-bin
     pkgs.python3
+    pulumi-esc
     # pulumi
     # pulumiPackages.pulumi-language-python
     # python311Packages.pulumi
-    (import ./nixpkgs/pulumi-esc.nix { inherit (pkgs) lib buildGoModule fetchFromGitHub; }) #pulumi-esc
+    # (import ./nixpkgs/pulumi-esc.nix { inherit (pkgs) lib buildGoModule fetchFromGitHub; }) #pulumi-esc
   ];
 }
+
+
+
+
