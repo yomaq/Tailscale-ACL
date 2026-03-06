25717c7e3a1838580c91a4e1f828007701061a32
yomaq <yomaq@bsjm.xyz>
2026-03-05 18:35:08 -0600

    update access

--- acl_modules/mulvad.py
+++ acl_modules/mulvad.py
@@ -1,9 +1,8 @@
 import pulumi_tailscale as tailscale
 
-
 # mulvad access
 hostnames = [
-    "gray",
+    "grayed",
     "midnight",
     "pewter",
     "beam",
@@ -13,7 +12,7 @@ hostnames = [
 devices = tailscale.get_devices()
 if devices and devices.devices:
     first_device = devices.devices[0]
-    tailnet_name = first_device.name.split('.')[1]
+    tailnet_name = first_device.name.split(".")[1]
 else:
     raise ValueError("No devices found in the tailnet")
 
@@ -22,5 +21,5 @@ for hostname in hostnames:
     # iOS devices hostname's don't list properly, so we use the full tailnet name instead.
     full_hostname = f"{hostname}.{tailnet_name}.ts.net"
     device = tailscale.get_device(name=full_hostname)
-    ip_address = device.addresses[0]  
+    ip_address = device.addresses[0]
     mulvad_access["nodeAttrs"].append({"target": [ip_address], "attr": ["mullvad"]})
